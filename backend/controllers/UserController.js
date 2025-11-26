f2968230d5fa96f8f3ce68e95fb8bbc24d6d4564
Nathan-VE <nvictoresposito@gmail.com>
2025-11-26 16:21:06 -0300

    feat: implement new validation rules

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -14,7 +14,36 @@ const generateToken = (id) => {
 
 // register user and sign in
 const register = async (req, res) => {
-    res.send("Registro");
+    const { name, email, password } = req.body;
+
+    // check if user exists (correção: verificar corretamente)
+    const user = await User.findOne({ email });
+    if (user) {
+        res.status(422).json({ errors: ["Por favor, utilize um e-mail válido."] });
+        return;
+    }
+
+    // generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create user (correção: User.create)
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash,
+    });
+
+    // if new user
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente novamente mais tarde."] });
+        return; // correção
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
 };
 
 module.exports = {
